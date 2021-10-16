92fcee4fa45eba955f145cc3b856afb5900d9742
Arya-Chandran <aryachandran86@gmail.com>
2021-10-16 00:41:19 -0400

    created comment list and a form to submit new comment, added globla partial

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,98 +1,89 @@
-
-const showDetails= [
-    {  
-       date: 'Mon Sept 06 2021',
-       venue: 'Ronald Lane',
-       location: 'San Francisco, CA'
-    },
-    {  
-        date: 'Tues Sept 21 2021',
-        venue: 'Pier 3 East',
-        location: 'San Francisco, CA'
-     },
-     {  
-        date: 'Fri Oct 15 2021',
-        venue: 'View Lounge',
-        location: 'San Francisco, CA'
-     },
-     {  
-        date: 'Sat Nov 06 2021',
-        venue: 'Hyatt Agency',
-        location: 'San Francisco, CA'
-     },
-     {  
-        date: 'Fri Nov 26 2021',
-        venue: 'Moscow Center',
-        location: 'San Francisco, CA'
-     },
-     {  
-        date: 'Wed Dec 15 2021',
-        venue: 'Press Club',
-        location: 'San Francisco, CA'
-     }
+const showDetails = [
+  {
+    date: "Mon Sept 06 2021",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Tues Sept 21 2021",
+    venue: "Pier 3 East",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Fri Oct 15 2021",
+    venue: "View Lounge",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Sat Nov 06 2021",
+    venue: "Hyatt Agency",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Fri Nov 26 2021",
+    venue: "Moscow Center",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Wed Dec 15 2021",
+    venue: "Press Club",
+    location: "San Francisco, CA",
+  },
 ];
 
-
-const showsList = document.querySelector('.shows__cards');
+const showsList = document.querySelector(".shows__cards");
 console.log(showsList);
 
-
-function displayShows(){
-    for(let i = 0; i < showDetails.length; i++){   
+function listShows() {
+  for (let i = 0; i < showDetails.length; i++) {
     // create <li>
-    const showsItem = document.createElement('li');
-    showsItem.classList.add('shows__card');
-    
+    const showsItem = document.createElement("li");
+    showsItem.classList.add("shows__card");
+
     //create <h4>
-    const dateTitle = document.createElement('h4');
-    dateTitle.innerText ='Date';
-    dateTitle.classList.add('shows__card--title');
-   
+    const dateTitle = document.createElement("h4");
+    dateTitle.innerText = "Date";
+    dateTitle.classList.add("shows__card--title");
+
     // create <p>
-    const showsDate = document.createElement('p');
+    const showsDate = document.createElement("p");
     showsDate.innerText = showDetails[i].date;
-    showsDate.classList.add('shows__card--content');
-  
-    const venueTitle = document.createElement('h4');
-    venueTitle.innerText ='Venue';
-    venueTitle.classList.add('shows__card--title');
+    showsDate.classList.add("shows__card--content");
 
-    const showsVenue = document.createElement('p');
+    const venueTitle = document.createElement("h4");
+    venueTitle.innerText = "Venue";
+    venueTitle.classList.add("shows__card--title");
+
+    const showsVenue = document.createElement("p");
     showsVenue.innerText = showDetails[i].venue;
-    showsVenue.classList.add('shows__card--content');
-    
- 
-    const locationTitle = document.createElement('h4');
-    locationTitle.innerText ='Location';
-    locationTitle.classList.add('shows__card--title');
+    showsVenue.classList.add("shows__card--content");
 
-    const showsLocation = document.createElement('p');
+    const locationTitle = document.createElement("h4");
+    locationTitle.innerText = "Location";
+    locationTitle.classList.add("shows__card--title");
+
+    const showsLocation = document.createElement("p");
     showsLocation.innerText = showDetails[i].location;
-    showsLocation.classList.add('shows__card--content');
-
+    showsLocation.classList.add("shows__card--content");
 
     // create <a> button
-    const showsBtn = document.createElement('a');
-    showsBtn.innerText = 'Buy Tickets';
-    showsBtn.setAttribute('href', '#');
-    showsBtn.classList.add('btn');
-  
+    const showsBtn = document.createElement("a");
+    showsBtn.innerText = "Buy Tickets";
+    showsBtn.setAttribute("href", "#");
+    showsBtn.classList.add("btn");
 
     // append create elements  to showsItem
     showsItem.appendChild(dateTitle);
     showsItem.appendChild(showsDate);
-    showsItem.appendChild(venueTitle)
+    showsItem.appendChild(venueTitle);
     showsItem.appendChild(showsVenue);
     showsItem.appendChild(locationTitle);
     showsItem.appendChild(showsLocation);
     showsItem.appendChild(showsBtn);
-    
+
     // finally append all the <li> elements to showsList
     showsList.appendChild(showsItem);
   }
 }
 
-displayShows();
-
-
-
+listShows();
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,57 +1,108 @@
-const commentDetails= [
-    {  
-       personName:'Connor Walton',
-       timeStamp: '02/17/2021',
-       comment: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains' 
-    },
-    {  
-        personName:'Emilie Beach',
-        timeStamp: '01/09/2021',
-        comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.' 
-     },
-     {  
-        personName:'Miles Acosta',
-        timeStamp: '12/20/2020',
-        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-     }  
+
+// create an array with 3 default comment objects
+const commentDetails = [
+  {
+    personName: "Connor Walton",
+    timeStamp: "02/17/2021",
+    comment:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains",
+  },
+  {
+    personName: "Emilie Beach",
+    timeStamp: "01/09/2021",
+    comment:
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    personName: "Miles Acosta",
+    timeStamp: "12/20/2020",
+    comment:
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
 ];
 
-const commentsList = document.querySelector('.comments__cards');
-console.log(commentsList);
+const commentsList = document.querySelector(".comments__cards");
 
+// create function that takes one comment object as a parameter
+function displayComment(newComment) {  
+  if (newComment) {
+    commentDetails.push(newComment); // added new comment to the array
+  }
+  const sortedComments = commentDetails.sort(function (a, b) {  // sort the objects in the array by date
+    return new Date(b.timeStamp) - new Date(a.timeStamp);
+  });
 
-
-function displayComment(){
-    for(let i = 0; i < commentDetails.length; i++){   
+  sortedComments.forEach(function (comment) {
     // create <li>
-    const commentsItem = document.createElement('li');
-    commentsItem.classList.add('comments__card');
-    
+    const commentsItem = document.createElement("li");
+    commentsItem.classList.add("comments__card");
+
+    //create <image>
+    const commentsImage = document.createElement("img");
+    commentsImage.classList.add("comments__card--image");
+
     //create <h4>
-    const commentsName = document.createElement('h4');
-    commentsName.innerText = commentDetails[i].personName;
-    commentsName.classList.add('comments__card--name');
+    const commentsName = document.createElement("h4");
+    commentsName.innerText = comment.personName;
+    commentsName.classList.add("comments__card--name");
 
     // create <p>
-    const commentsDate = document.createElement('p');
-    commentsDate.innerText = commentDetails[i].timeStamp;
-    commentsDate.classList.add('comments__card--date');
-  
+    const commentsDate = document.createElement("p");
+    commentsDate.innerText = new Date(comment.timeStamp) .toLocaleString() .split(",")[0];
+    commentsDate.classList.add("comments__card--date");
 
-    const commentsText = document.createElement('p');
-    commentsText.innerText = commentDetails[i].comment;
-    commentsText.classList.add('comments__card--text');
-
+    const commentsText = document.createElement("p");
+    commentsText.innerText = comment.comment;
+    commentsText.classList.add("comments__card--text");
 
     // append create elements  to commentsItem
+    commentsItem.appendChild(commentsImage);
     commentsItem.appendChild(commentsName);
     commentsItem.appendChild(commentsDate);
-    commentsItem.appendChild(commentsText); 
+    commentsItem.appendChild(commentsText);
 
-
-    // finally append all the <li> elements to showsList
+    // finally append all the <li> elements to commentsList
     commentsList.appendChild(commentsItem);
-  }
+  });
 }
 
+// Get timestamp in mm/dd/yyyy format
+function getCurrentDate() {
+  const currentDate = new Date(Date.now());
+  return currentDate;
+}
+
+function createCommentObject(name, comment) {
+  return {
+    personName: name,
+    timeStamp: getCurrentDate(),
+    comment: comment,
+  };
+}
+
+//clears the input fields after submitting a new comment
+function clear() {
+  document.getElementById("name").value = "";
+  document.getElementById("comment").value = "";
+}
+
+// Event Listener for handling form submission
+const form = document.getElementById("commentForm");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const name = e.target.name.value;
+  const comment = e.target.comment.value;
+  let commentSection = document.querySelector(".comments__cards");
+
+  //creating new comment object
+  const newComment = createCommentObject(name, comment);
+
+  //Clearing the list from DOM and adding the updated comment list
+  commentSection.innerHTML = "";
+  displayComment(newComment);
+  clear();
+});
+
+// Listing the comments in first render
 displayComment();
